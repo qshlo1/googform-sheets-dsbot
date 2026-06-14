8f2c450eb9350bcd8e9ac9527d26b8a3517b2668
qshlo1 <s82600937@gmail.com>
2026-06-14 12:32:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
+import express from "express";
+
+const app = express();
+app.use(express.json());
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
@@ -6,15 +10,30 @@ const client = new Client({
 
 client.once("ready", async () => {
   console.log(`Бот запущен: ${client.user.tag}`);
+});
+
+app.post("/form", async (req, res) => {
+  const data = req.body;
 
   const channel = await client.channels.fetch(process.env.CHANNEL_ID);
 
-  if (!channel) {
-    console.log("Канал не найден");
-    return;
-  }
+  const embed = new EmbedBuilder()
+    .setTitle("📩 Отчёт на повышение")
+    .setColor(0x00ff99)
+    .addFields(
+      { name: "Имя", value: data.static || "—", inline: true },
+      { name: "Возраст", value: data.udo || "—", inline: true },
+      { name: "Возраст", value: data.exam || "—", inline: true },
+      { name: "Комментарий", value: data.pmp || "—" }
+    );
 
-  channel.send("✅ Бот успешно запущен и работает 24/7");
+  channel.send({ embeds: [embed] });
+
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("API сервер запущен");
 });
 
 client.login(process.env.DISCORD_TOKEN);
