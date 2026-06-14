7c04fcb49731e5d89573b1104108c94663e862ce
qshlo1 <s82600937@gmail.com>
2026-06-14 11:29:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log(`Бот запущен: ${client.user.tag}`);
+
+  const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+
+  if (!channel) {
+    console.log("Канал не найден");
+    return;
+  }
+
+  channel.send("✅ Бот успешно запущен и работает 24/7");
 });
 
 client.login(process.env.DISCORD_TOKEN);
