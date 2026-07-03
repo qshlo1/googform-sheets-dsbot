8a22dc049dc996653866c5abc1ce0b1438694e68
qshlo1 <s82600937@gmail.com>
2026-07-03 16:22:59 +0500

    Update promotion.js

--- handlers/promotion.js
+++ handlers/promotion.js
@@ -1,1 +1,35 @@
+export async function sendPromotion(channel, data) {
 
+    const { staticId, rankFromTo, reportLink } = data;
+
+    await channel.send({
+        content: "<@&1512785654084534413>",
+        embeds: [
+            {
+                title: "📋 Запрос на повышение",
+                color: 0xFFC107,
+
+                fields: [
+                    {
+                        name: "👤 Имя Фамилия | #StaticID",
+                        value: String(staticId || "—"),
+                        inline: false
+                    },
+                    {
+                        name: "📈 С какого на какой ранг",
+                        value: String(rankFromTo || "—"),
+                        inline: false
+                    },
+                    {
+                        name: "📄 Ссылка на отчёт",
+                        value: String(reportLink || "—"),
+                        inline: false
+                    }
+                ],
+
+                timestamp: new Date().toISOString()
+            }
+        ]
+    });
+
+}
