626c3c650541014e07525209b9bb5715d8702bbe
qshlo1 <s82600937@gmail.com>
2026-07-03 19:51:37 +0500

    Update weeklyReport.js

--- handlers/weeklyReport.js
+++ handlers/weeklyReport.js
@@ -40,7 +40,7 @@ export async function sendWeeklyReport(channel, data) {
               value: String(balls || "—"),
               inline: false
             }
-          }
+        }
         ],
 
 
