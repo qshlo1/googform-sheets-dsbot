4b7c12574c7b0513b0bccf41c9e2ffbd1bb29823
qshlo1 <s82600937@gmail.com>
2026-07-03 16:42:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.post("/form", async (req, res) => {
          return res.status(400).send("Unknown form type");
 
       }
-}
+     }
 
   console.log("✅ Embed sent");
   console.log("━━━━━━━━━━━━━━━━━━━━");
